483fe9f75d232505a02fa312e9ca621d6fb829ea
alperenkavukk <kavuk.67.76@gmail.com>
2022-10-09 18:58:00 +0300

    Initial Commit

---
+++ swiftUI_mvvmapp/swiftUI_mvvmappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_mvvmappApp.swift
+//  swiftUI_mvvmapp
+//
+//  Created by Alperen Kavuk on 9.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_mvvmappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
